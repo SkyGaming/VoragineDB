2a76ca7975a2ad552863f30e5f66629e5beb197d
Kitiara <vinas_solamnus1@hotmail.com>
2011-08-31 09:27:04 +0300

    Updated the opcodes and added new column & table for characters.
    Signed-off-by: Kitiara <vinas_solamnus1@hotmail.com>

---
+++ Base Updates(Just Urgents)/Characters & Auth/00001_added_new_character_column_and_table.sql
@@ -0,0 +1,13 @@
+ALTER TABLE `characters` ADD COLUMN `grantableLevels` tinyint(3) UNSIGNED NOT NULL DEFAULT 0 AFTER `actionBars`;
+CREATE TABLE `account_tutorial` (
+  `accountId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Account Identifier',
+  `tut0` int(10) unsigned NOT NULL DEFAULT '0',
+  `tut1` int(10) unsigned NOT NULL DEFAULT '0',
+  `tut2` int(10) unsigned NOT NULL DEFAULT '0',
+  `tut3` int(10) unsigned NOT NULL DEFAULT '0',
+  `tut4` int(10) unsigned NOT NULL DEFAULT '0',
+  `tut5` int(10) unsigned NOT NULL DEFAULT '0',
+  `tut6` int(10) unsigned NOT NULL DEFAULT '0',
+  `tut7` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`accountId`)
+) ENGINE=InnoDB;
